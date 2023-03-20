077d4748e09ba48bc1cfc0ba67543a6051f657da
Marc Hamilton <unionhills@cox.net>
2023-03-19 18:18:41 -0700

    Moved exercises from individual repos to this one.

---
+++ blitz/src/pages/index.tsx
@@ -0,0 +1,68 @@
+import { Suspense } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import Layout from "src/core/layouts/Layout"
+import { useCurrentUser } from "src/users/hooks/useCurrentUser"
+import logout from "src/auth/mutations/logout"
+import logo from "public/logo.png"
+import { useMutation } from "@blitzjs/rpc"
+import { Routes, BlitzPage } from "@blitzjs/next"
+
+/*
+ * This file is just for a pleasant getting started page for your new app.
+ * You can delete everything in here and start from scratch if you like.
+ */
+
+const UserInfo = () => {
+  const currentUser = useCurrentUser()
+  const [logoutMutation] = useMutation(logout)
+
+  if (currentUser) {
+    return (
+      <>
+        <button
+          className="button small"
+          onClick={async () => {
+            await logoutMutation()
+          }}
+        >
+          Logout
+        </button>
+        <div>
+          User id: <code>{currentUser.id}</code>
+          <br />
+          User role: <code>{currentUser.role}</code>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <Link href={Routes.SignupPage()}>
+          <a className="button small">
+            <strong>Sign Up</strong>
+          </a>
+        </Link>
+        <Link href={Routes.LoginPage()}>
+          <a className="button small">
+            <strong>Login</strong>
+          </a>
+        </Link>
+      </>
+    )
+  }
+}
+
+const Home: BlitzPage = () => {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+
+      <Suspense fallback="Loading...">
+        <UserInfo />
+      </Suspense>
+    </div>
+  )
+}
+
+export default Home
