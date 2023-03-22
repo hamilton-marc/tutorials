f1895a86b294b8ec977650e59c123697a01c414d
Marc Hamilton <unionhills@cox.net>
2023-03-21 21:01:19 -0700

    build hilla view using components

--- hilla-crm/frontend/views/list/list-view.ts
+++ hilla-crm/frontend/views/list/list-view.ts
@@ -1,24 +1,41 @@
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { View } from '../../views/view';
+import '@vaadin/text-field';
+import '@vaadin/button';
+import '@vaadin/grid';
+import '@vaadin/grid/vaadin-grid-column';
 
 @customElement('list-view')
 export class ListView extends View {
   render() {
-    return html`<h1>Hello, world!</h1>`;
+    return html`
+      <div class="toolbar flex gap-s">
+        <vaadin-text-field placeholder="Filter by name" clear-button-visible></vaadin-text-field>
+        <vaadin-button>Add Contact</vaadin-button>
+      </div>
+      <div class="content flex gap-m h-full">
+        <vaadin-grid class="grid h-full">
+          <vaadin-grid-column path="firstName" auto-width> </vaadin-grid-column>
+          <vaadin-grid-column path="lastName" auto-width> </vaadin-grid-column>
+          <vaadin-grid-column path="email" auto-width> </vaadin-grid-column>
+          <vaadin-grid-column path="status.name" header="Status" auto-width></vaadin-grid-column>
+          <vaadin-grid-column path="company.name" header="Company" auto-width></vaadin-grid-column>
+        </vaadin-grid>
+      </div>
+    `;
   }
 
   connectedCallback() {
     super.connectedCallback();
     this.classList.add(
+      'box-border',
       'flex',
       'flex-col',
-      'h-full',
-      'items-center',
-      'justify-center',
-      'p-l',
-      'text-center',
-      'box-border'
+      'p-m',
+      'gap-s',
+      'w-full',
+      'h-full'
     );
   }
 }
