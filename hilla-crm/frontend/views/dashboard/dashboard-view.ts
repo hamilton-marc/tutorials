30e768894ad1a2bd1a2a9768c5975fc84f37cfaa
Marc Hamilton <unionhills@cox.net>
2023-04-29 22:40:51 -0700

    add dashboard with chart
    replaced vaadin chart with google chart since vaadin chart requires a
    license.

--- hilla-crm/frontend/views/dashboard/dashboard-view.ts
+++ hilla-crm/frontend/views/dashboard/dashboard-view.ts
@@ -1,6 +1,8 @@
 import { html } from 'lit';
 import { customElement } from 'lit/decorators';
 import { View } from 'Frontend/views/view';
+import { dashboardViewStore } from './dashboard-view-store';
+import '@google-web-components/google-chart';
 
 @customElement('dashboard-view')
 export class DashboardView extends View {
@@ -9,7 +11,32 @@ export class DashboardView extends View {
     this.classList.add('flex', 'flex-col', 'items-center', 'pt-xl');
   }
 
+  getChartData() {
+    const axes = ['["Company", "Count"]']
+    const data = dashboardViewStore.companyStats.map(stat => '["' + stat.name + '",' + stat.y + ']');
+
+    return axes.concat(data);
+  }
+
+  getCompanyStats() {
+    if (dashboardViewStore.companyStats.length === 0) {
+      return html`<p>Loading stats...</p>`;
+    } else {
+      return html`
+        <google-chart type="pie"
+          options='{ "pieHole": 0.3 }'
+          data='[${this.getChartData().toString()}]'></google-chart>
+      `;
+    }
+  }
+
   render() {
-    return html`<h1>Dashboard view</h1>`;
+    return html`
+    <div class="text-xl mb-xl">
+      ${dashboardViewStore.contactCount} contacts
+    </div>
+    
+    ${this.getCompanyStats()}
+  `;
   }
 }
